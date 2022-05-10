f68e4bb186f1df063e0fc245de8b312643a179e5
tim <tfierek357@gmail.com>
2022-05-09 21:43:16 -0400

    initial commit, basic VHS and DVD functionality

---
+++ BlockbusterLab/src/Blockbuster.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Blockbuster contains the io functionality and main method of the app.
+ * 
+ * @author Tim Fierek
+ *
+ */
+public class Blockbuster {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		ArrayList<Movie> movies = new ArrayList<>();
+		
+	}
+	
+}
